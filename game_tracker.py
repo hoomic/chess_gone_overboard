5bdc39a9826d6a95e1811c927b9946ded57501cb
Michael Hooi <michael.r.hooi@gmail.com>
2021-05-26 08:25:29 -0400

    Add CNN classifier to detect if a square has a piece

--- game_tracker.py
+++ game_tracker.py
@@ -1,5 +1,6 @@
 import chess
 from chesscv import BoardDetector, show_wait_destroy
+from piece_classifier import train_piece_classifier, get_piece_prediction
 
 import os
 # if this is running on a pi, then import picamera
@@ -23,8 +24,8 @@ def track_game(video):
     time.sleep(2.0)
     camera.capture(rawCapture, format="bgr")
     first_frame = rawCapture.array
-    params = board_detector.locate_squares(first_frame)
-    move_tracker.set_square_coordinates(params)
+    params, ps = board_detector.locate_squares(first_frame)
+    move_tracker.set_square_coordinates(params, ps)
     rawCapture.truncate(0)
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
       frame = board_detector.transform_image(frame)
@@ -37,16 +38,24 @@ def track_game(video):
     while not board_found:
       _, first_frame = vs.read()
       try:
-        params = board_detector.locate_squares(first_frame)
-        move_tracker.set_square_coordinates(params)
+        params, ps = board_detector.locate_squares(first_frame)
+        move_tracker.set_square_coordinates(params, ps)
+        move_tracker.set_first_frame(board_detector.transform_image(first_frame))
         board_found = True
       except TypeError:
         continue
+    classifier_trained = False
     while (vs.isOpened()):
       ret, frame = vs.read()
       if not ret:
         break
+      cv.imshow('frame', frame)
       frame = board_detector.transform_image(frame)
+      if cv.waitKey(100) & 0xFF == ord('q'):
+        break
+      if not classifier_trained:
+        move_tracker.train_piece_classifier(frame)
+        classifier_trained = True
       move_tracker.process_image(frame)
 
 class Square():
@@ -56,31 +65,11 @@ class Square():
   def __init__(self, board_coords, image_coords):
     self.board_coords = board_coords
     self.image_coords = image_coords
-    x0, x1, y0, y1 = image_coords
-    tile_size = ((x1 - x0) + (y1 - y0)) // 10
-    self.clahe = cv.createCLAHE(clipLimit=10.0, tileGridSize=(tile_size, tile_size))
-    self.piece = '.'
-    self.frames = []
 
-  def detect_difference(self, prev_image, curr_image):
-    last_square = self.get_image(prev_image)
-    if not len(self.frames):
-      self.frames.append(last_square)
-    square = self.get_image(curr_image)
-    self.frames.append(square)
-    w, h = square.shape
-    delta = cv.absdiff(last_square, square)
-    corr = np.corrcoef(last_square.flatten(), square.flatten())[0,1]
-    center_delta = delta[w//3:(2*w)//3, h//3:(2*h)//3]
-    if np.median(delta) > 18: #TODO make this more robust
-      self.display_frames()
-      cv.imshow('last', last_square)
-      cv.imshow('curr', square)
-      cv.imshow('{},{},{}'.format(*np.percentile(center_delta, [50,100]), np.mean(center_delta)), center_delta)
-      show_wait_destroy('{},{},{}'.format(*np.percentile(delta, [50,100]), s), delta)
-      cv.destroyWindow('{},{},{}'.format(*np.percentile(center_delta, [50,100]), np.mean(center_delta)))
-      return self.board_coords
-    return None
+  def get_delta(self, prev_frame, curr_frame):
+    prev_image = self.get_image(prev_frame)
+    curr_image = self.get_image(curr_frame)
+    return cv.absdiff(prev_image, curr_image)
 
   def get_image(self, image):
     x0, x1, y0, y1 = self.image_coords
@@ -89,13 +78,6 @@ class Square():
   def display(self, image):
     square = self.get_image(image)
     show_wait_destroy("{},{}".format(*self.board_coords), square)
-
-  def display_frames(self):
-    if len(self.frames) < 10:
-      return
-    for i, frame in enumerate(self.frames[-10:]):
-      cv.imshow(str(i), frame)
-    cv.waitKey(0)
 
 class MoveTracker():
   """
@@ -116,109 +98,180 @@ class MoveTracker():
 
     # compute the absolute difference between the previous frame and the current frame
     frame_delta = cv.absdiff(self.prev_frame, frame)
-    if np.all(frame_delta < 40): #image is still
-      #show_wait_destroy('still', frame)
-      if self.last_still_frame is None:
-        self.last_still_frame = frame
-        return
+    first_delta = cv.absdiff(self.last_still_frame, frame)
+    if np.all(frame_delta < 40) and np.percentile(first_delta, 90) < 50: #image is still
+      #show_wait_destroy('still: {0:.2f}'.format(np.percentile(first_delta, 90)), frame)
       # so see if the board differs from the last still board
-      square_changes = []
-      for square in self.squares:
-        diff = square.detect_difference(self.last_still_frame, frame)
-        if diff is not None:
-          square_changes.append(diff)
-      if not len(square_changes):
-        return
-      elif len(square_changes) == 1:
-        # When a move occurs, at least two squares should change
-        import pdb; pdb.set_trace()
-        return
-      elif len(square_changes) > 4:
-        # If more than 4 squares change, this is also not possible
-#        import pdb; pdb.set_trace()
-        return
-      else:
-        move = self.process_move(square_changes)
-        if move is not None:
-          show_wait_destroy(move.uci(), frame)
-          print(self.board)
-          print('-' * 50)
-      self.last_still_frame = frame
+      move_candidates = []
+      for move in self.board.legal_moves:
+        if self.white_side is None:
+          #first calculate as if it is left side, then right side
+          from_square = self.get_square(move.from_square, 'left') 
+          to_square = self.get_square(move.to_square, 'left')
+          if self.detect_move(from_square, to_square, frame):
+            self.white_side = 'left'
+            print("White side is left")
+            move_candidates.append(move)
+          from_square = self.get_square(move.from_square, 'right')
+          to_square = self.get_square(move.to_square, 'right')
+          if self.detect_move(from_square, to_square, frame):
+            self.white_side = 'right'
+            print("White is is right")
+            move_candidates.append(move)
+        else:
+          from_square = self.get_square(move.from_square)
+          to_square = self.get_square(move.to_square)
+          if self.detect_move(from_square, to_square, frame):
+            move_candidates.append(move)
+      move_made = None
+      if len(move_candidates) == 1:
+        move_made = move_candidates[0]
+      elif len(move_candidates) > 1:
+        move_made = self.check_for_castle(move_candidates)
+        if not move_made:
+          move_made = filter_multiple_moves
+          if not move_made: 
+            import pdb; pdb.set_trace()
+      if move_made is not None:
+        self.board.push(move_made)
+        print(self.board)
+        print('-' * 50)
+        show_wait_destroy(move_made.uci(), frame)
+        self.last_still_frame = frame
     self.prev_frame = frame
 
-  def process_move(self, square_changes):
-    if self.white_side is None:
-      #this should be the first move
-      if np.all([s[1] <= 4 for s in square_changes]):
-        self.white_side = 'left'
-      elif np.all([s[1] > 4 for s in square_changes]):
-        self.white_side = 'right'
-      else:
-        import pdb; pdb.set_trace()
-    move = None
-    square_changes = [self.get_square(s) for s in square_changes]
-    if len(square_changes) == 2: # "normal" move
-      s0, s1 = square_changes
-      try:
-        move = self.board.find_move(s0, s1)
-      except ValueError:
-        try:
-          move = self.board.find_move(s1, s0)
-        except ValueError:
-          # if a move is not identified, it is probably just noise
-          #import pdb; pdb.set_trace()
-          return
-    elif len(square_changes) == 3: # en passant move
-      if not self.board.has_legal_en_passant():
-        import pdb; pdb.set_trace()
-        raise InvalidMove("En passant attempted when not legal")
-      ep_square = chess.Square(self.board.ep_square)
-      try:
-        s_dest = [s for s in square_changes if s == ep_square][0]
-      except:
-        import pdb; pdb.set_trace()
-      s0, s1 = [s for s in square_changes if s != ep_square]
-      try:
-        move = self.board.find_move(s0, s_dest)
-      except ValueError:
-        move = self.board.find_move(s1, s_dest)
-    elif len(square_changes) == 4: # castle
-      if self.board.turn == chess.WHITE:
-        king_square = chess.square(4,0)
-        if chess.square(6,0) in square_changes:
-          move = self.board.find_move(king_square, chess.square(6,0))
-        elif chess.square(2,0) in square_changes:
-          move = self.board.find_move(king_square, chess.square(2,0))
-        else:
-          import pdb; pdb.set_trace()
-      else:
-        king_square = chess.square(4,7)
-        if chess.square(6,7) in square_changes:
-          move = self.board.find_move(king_square, chess.square(6,7))
-        elif chess.square(2,7) in square_changes:
-          move = self.board.find_move(king_square, chess.square(2,7))
-    if move is None:
-      return
-    self.board.push(move)
-    return move
-
-  def get_square(self, square_id):
-    if self.white_side == 'left':
-      return chess.square(square_id[1], square_id[0])
+  def check_for_castle(self, move_candidates):
+    if self.board.turn == chess.WHITE:
+      if chess.Move.from_uci('e1g1') in move_candidates:
+        return chess.Move.from_uci('e1g1')
+      elif chess.Move.from_uci('e1c1') in move_candidates:
+        return chess.Move.from_uci('e1c1')
     else:
-      return chess.square(7-square_id[1], 7-square_id[0])
+      if chess.Move.from_uci('e8g8') in move_candidates:
+        return chess.Move.from_uci('e8g8')
+      elif chess.Move.from_uci('e8c8') in move_candidates:
+        return chess.Move.from_uci('e8c8')
+    return None
 
-  def set_square_coordinates(self, params):
+  def detect_move(self, from_square, to_square, frame):
+    from_pred = self.get_piece_prediction(from_square.get_image(frame))
+    to_pred = self.get_piece_prediction(to_square.get_image(frame))
+    from_delta = from_square.get_delta(self.last_still_frame, frame)
+    to_delta = to_square.get_delta(self.last_still_frame, frame)
+    if np.median(from_delta) < 10 or np.median(to_delta) < 10:
+      return False
+    if from_pred < -2 and to_pred > 0:
+#      cv.imshow('from_curr: {0:.3f}'.format(from_pred), from_square.get_image(frame))
+#      show_wait_destroy('to_curr: {0:.3f}'.format(to_pred), to_square.get_image(frame))
+      return True
+    # if the from square definitely doesn't have a piece, then take a closer look at
+    # the to squares
+    elif from_pred < -3 and np.median(from_delta) > 25: 
+      print("Taking a closer look at to_square")
+      to_pred = self.get_piece_prediction(to_square.get_image(frame), True)
+#      cv.imshow('from_curr: {0:.3f}'.format(from_pred), from_square.get_image(frame))
+#      show_wait_destroy('to_curr: {0:.3f}'.format(to_pred), to_square.get_image(frame))
+      if to_pred > 0 and np.median(to_delta) > 25:
+        return True
+    return False
+
+  def filter_multiple_moves(self, move_candidates):
+    # if all moves have the same from_square, then just look at the to squares
+    if np.all([m.from_square == move_candidates[0].from_square for m in move_candidates]):
+      deltas = []
+      for move in move_candidates:
+        to_square = self.get_square(move.to_square)
+        to_delta = to_square.get_delta(self.last_still_frame, frame)
+        deltas.append(np.median(to_delta))
+      # if one delta median is 2x greater than the rest, then chose that one
+      if np.all([np.max(deltas) > 2*d for i, d in enumerate(deltas) if i != np.argmax(deltas)]):
+        return move_candidates[np.argmax(deltas)]
+
+  def get_square(self, square_id, side=None):
+    if side is None:
+      side = self.white_side
+    if side == 'left':
+      return self.squares[square_id]
+    else:
+      return self.squares[63 - square_id]
+
+  def set_square_coordinates(self, params, perspective_shift):
     self.squares = []
     x, y ,s = params
+    p0 = perspective_shift[0]
     for i in range(8):
-      x_lo = i * s
-      x_hi = (i + 1) * s
+      p_i = min(i, 7-i)
+      lo_shift = -perspective_shift[p_i] if i < 4 else perspective_shift[p_i + 1]
+      hi_shift = -perspective_shift[p_i + 1] if i < 4 else perspective_shift[p_i]
+      x_lo = i * s + lo_shift + p0
+      x_hi = (i + 1) * s + hi_shift + p0
       for j in range(8):
-        y_lo = j * s
-        y_hi = (j + 1) * s
+        p_j = min(j, 7-j)
+        lo_shift = -perspective_shift[p_j] if j < 4 else perspective_shift[p_j + 1]
+        hi_shift = -perspective_shift[p_j + 1] if j < 4 else perspective_shift[p_j]
+        y_lo = j * s + lo_shift + p0
+        y_hi = (j + 1) * s + hi_shift + p0
         self.squares.append(Square((i, j), (x_lo, x_hi, y_lo, y_hi)))
 
+  def set_first_frame(self, frame):
+    self.last_still_frame = frame
+  
+  def train_piece_classifier(self, frame):
+    images = []
+    labels = []
+    for i, square in enumerate(self.squares):
+      try:
+        image = cv.resize(square.get_image(frame), (32, 32), interpolation=cv.INTER_AREA)
+      except:
+        import pdb; pdb.set_trace()
+      image = image/255
+      label = 1 if i < 16 or i > 47 else 0
+      for image in generate_transformations(image):
+        images.append(image)
+        labels.append(label)
+    images = np.array(images)
+    images = images.reshape(-1, 1, 32, 32)
+    labels = np.array(labels)
+    labels = labels.reshape(-1, 1)
+    self.piece_classifier = train_piece_classifier(images, labels)
+
+  def get_piece_prediction(self, orig_image, transform = False):
+    image = np.copy(orig_image)
+    image = cv.resize(image, (32, 32), interpolation=cv.INTER_AREA)
+    image = image/255
+    if transform:
+      preds = []
+      for image in generate_transformations(image, True):
+        image = image.reshape(1, 1, 32, 32)
+        pred = get_piece_prediction(self.piece_classifier, image)
+        preds.append(get_piece_prediction(self.piece_classifier, image))
+      return np.max(preds)
+    else:
+      image = image.reshape(1, 1, 32, 32)
+      return get_piece_prediction(self.piece_classifier, image)
+
+def generate_transformations(image, roll=False):
+  """ function to make mirror and rotation transformations """
+  for j in range(4):
+    new_image = np.copy(image)
+    rotation_matrix = cv.getRotationMatrix2D((16, 16), j * 90, 1.0)
+    new_image = cv.warpAffine(new_image, rotation_matrix, (32, 32))
+    yield new_image
+  new_image = np.copy(image)
+  new_image = cv.flip(new_image, 0)
+  for j in range(4):
+    new_image = np.copy(new_image)
+    rotation_matrix = cv.getRotationMatrix2D((16, 16), j * 90, 1.0)
+    new_image = cv.warpAffine(new_image, rotation_matrix, (32, 32))
+    yield new_image
+  if roll:
+    for i in range(6):
+      new_image = np.copy(image)
+      new_image = np.roll(new_image, i, 0)
+      for j in range(6):
+        new_image = np.roll(new_image, j, 1)
+        yield new_image
+  
 
 if __name__ == "__main__":
   import argparse
---
+++ piece_classifier.py
@@ -0,0 +1,79 @@
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader, Dataset, TensorDataset
+import torchvision
+from torchvision import transforms
+import torch.nn.functional as F
+
+device = 'cpu'
+
+class PieceClassifier(nn.Module):
+    """Binary classifier to determine whether a square has a piece on it or not"""
+    def __init__(self):
+        super(PieceClassifier, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 128)
+        self.fc2 = nn.Linear(128, 1)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+def train_piece_classifier(images, labels):
+    model = PieceClassifier().to(device)
+
+    images = torch.Tensor(images)
+    labels = torch.Tensor(labels)
+    dataset = TensorDataset(images, labels) # create datset
+    dataloader = DataLoader(dataset, batch_size=512)
+
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    n = len(dataset)
+
+    for epoch in range(1000):  # loop over the dataset multiple times
+        #TODO break once we reach 100% accuracy
+        running_loss = 0.0
+        correct = 0
+        for i, data in enumerate(dataloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            #import pdb;pdb.set_trace()
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            pred = outputs[:, 0] > 0
+            l = labels[:, 0] == 1
+            correct = torch.sum(pred == l).item()
+
+            # print statistics
+            running_loss += loss.item()
+        print('[%d, %5d] loss: %.3f acc: %3f' %
+            (epoch + 1, i + 1, running_loss, correct/n))
+        if correct == n:
+            print("Training complete")
+            break
+        running_loss = 0.0
+    return model
+
+def get_piece_prediction(model, image):
+    image = torch.Tensor(image)
+    return model(image)[0][0].item()
+
+
+
+    
